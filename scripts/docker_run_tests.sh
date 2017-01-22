2cf4bb967704b204b3975f9506042134467afdfa
Matthieu Muffato <muffato@ebi.ac.uk>
2017-01-22 20:52:18 +0000

    trying something

--- scripts/docker_run_tests.sh
+++ scripts/docker_run_tests.sh
@@ -10,4 +10,18 @@ apt-get install -y sqlite3
 
 #sudo -u sgeadmin -E
 source /home/travis/build/muffato/ensembl-hive-sge/scripts/travis_run_tests.sh
+TRAVIS_DIR=/home/travis
+BUILD_DIR=$TRAVIS_DIR/build/muffato/ensembl-hive-sge/
+cd $BUILD_DIR
+export EHIVE_ROOT_DIR=$PWD/ensembl-hive
+export PERL5LIB=$EHIVE_ROOT_DIR/modules:$PWD/modules
+export EHIVE_TEST_PIPELINE_URLS='sqlite:///ehive_test_pipeline_db'
+export PATH=$TRAVIS_DIR/perl5/perlbrew/bin:$TRAVIS_DIR/perl5/perlbrew/perls/5.10/bin:$TRAVIS_DIR/bin::$TRAVIS_DIR/.local/bin::$PATH
+export HOME=/home/sgeadmin
+
+echo print_env
+sudo -u sgeadmin -E env
+echo before
+sudo -u sgeadmin -E prove -rv $BUILD_DIR/t
+echo after
 
