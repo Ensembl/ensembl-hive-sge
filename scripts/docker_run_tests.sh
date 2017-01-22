c15e3381f16fbe03eeef80bd92dc31a73d8fa0e1
Matthieu Muffato <muffato@ebi.ac.uk>
2017-01-22 21:57:30 +0000

    /home/travis/bin doesn't exist

--- scripts/docker_run_tests.sh
+++ scripts/docker_run_tests.sh
@@ -25,7 +25,7 @@ cd $BUILD_DIR
 export EHIVE_ROOT_DIR=$PWD/ensembl-hive
 export PERL5LIB=$EHIVE_ROOT_DIR/modules:$PWD/modules
 export EHIVE_TEST_PIPELINE_URLS='sqlite:///ehive_test_pipeline_db'
-export PATH=$TRAVIS_DIR/perl5/perlbrew/perls/5.10/bin:$TRAVIS_DIR/bin::$TRAVIS_DIR/.local/bin::$PATH
+export PATH=$TRAVIS_DIR/perl5/perlbrew/perls/5.10/bin:$TRAVIS_DIR/.local/bin:$PATH
 export HOME=/home/sgeadmin
 
 echo print_env
