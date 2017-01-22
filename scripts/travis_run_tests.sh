be8fcaafb0ba8c2b2eb28547863d282bcd4ee5d3
Matthieu Muffato <muffato@ebi.ac.uk>
2017-01-22 18:08:38 +0000

    Only "prove" should be run as sgeadmin

--- scripts/travis_run_tests.sh
+++ scripts/travis_run_tests.sh
@@ -16,5 +16,5 @@ export PERL5LIB=$EHIVE_ROOT_DIR/modules:$PWD/modules
 export EHIVE_TEST_PIPELINE_URLS='sqlite:///ehive_test_pipeline_db'
 export PATH=$TRAVIS_DIR/perl5/perlbrew/bin:$TRAVIS_DIR/perl5/perlbrew/perls/5.10/bin:$TRAVIS_DIR/bin::$TRAVIS_DIR/.local/bin::$PATH
 
-prove -rv t
+sudo -u sgeadmin -E prove -rv t
 
