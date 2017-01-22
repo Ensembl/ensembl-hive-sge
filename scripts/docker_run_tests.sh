3f9de602d0fb8b017aeae7518ade8dcc3264679d
Matthieu Muffato <muffato@ebi.ac.uk>
2017-01-22 19:07:35 +0000

    Perhaps without "exec" ? (set -e is used anyway)

--- scripts/docker_run_tests.sh
+++ scripts/docker_run_tests.sh
@@ -8,5 +8,5 @@ apt-get update
 apt-get install -y sqlite3
 #apt-get install -y sqlite3 libdbd-sqlite3-perl libdbi-perl libcapture-tiny-perl libxml-simple-perl libdatetime-perl libjson-perl libtest-exception-perl perl-modules libtest-warn-perl
 
-exec sudo -u sgeadmin -E /home/travis/build/muffato/ensembl-hive-sge/scripts/travis_run_tests.sh
+sudo -u sgeadmin -E /home/travis/build/muffato/ensembl-hive-sge/scripts/travis_run_tests.sh
 
