fffe792cfeb43ba7d84ad62f02bf5b6e57e02180
Matthieu Muffato <muffato@ebi.ac.uk>
2017-07-12 12:02:39 +0100

    Added Proc::Daemon to the list of dependencies

--- travisci/setup_docker_and_run_tests.sh
+++ travisci/setup_docker_and_run_tests.sh
@@ -10,7 +10,7 @@ echo "DEBUG: Environment of $0"; env; id; echo "END_DEBUG"
 
 # Install some packages inside the container
 apt-get update
-apt-get install -qqy sqlite3 libdbd-sqlite3-perl libdbi-perl libcapture-tiny-perl libxml-simple-perl libdatetime-perl libjson-perl libtest-exception-perl perl-modules libtest-warn-perl
+apt-get install -qqy sqlite3 libdbd-sqlite3-perl libdbi-perl libcapture-tiny-perl libxml-simple-perl libdatetime-perl libjson-perl libtest-exception-perl perl-modules libtest-warn-perl libproc-daemon-perl
 
 # It seems that non-root users cannot execute anything from /home/travis
 # so we copy the whole directory for the sgeadmin user
