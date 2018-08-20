78ab2b706968783c9ba8e11002d727cafa080b81
Matthieu Muffato <muffato@ebi.ac.uk>
2018-08-20 22:47:26 +0100

    More branch updates

--- scripts/ensembl-hive-sge/start_test_docker.sh
+++ scripts/ensembl-hive-sge/start_test_docker.sh
@@ -2,7 +2,7 @@
 
 HIVE_SGE_LOCATION=$1
 EHIVE_LOCATION=$2
-DOCKER_NAME=${3:-ensemblorg/ensembl-hive-sge:2.4}
+DOCKER_NAME=${3:-ensemblorg/ensembl-hive-sge:latest}
 
 exec docker run -it -v "$EHIVE_LOCATION:/repo/ensembl-hive" -v "$HIVE_SGE_LOCATION:/repo/ensembl-hive-sge" "$DOCKER_NAME"
 
